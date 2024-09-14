6dd382d38ef3a4556b169fd61791d2591447d86e
Hauries <hectories_15@hotmail.com>
2024-09-14 18:02:29 -0500

    debuging some features

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,18 +1,42 @@
 function loadHeaderFooter(){
-    var headerVar = new XMLHttpRequest();
-    headerVar.open("GET", "header.html", true);
-    headerVar.onreadystatechange = function () {
+  var headerVar = new XMLHttpRequest();
+  headerVar.open("GET", "header.html", true);
+  headerVar.onreadystatechange = function () {
       if (headerVar.readyState == 4 && headerVar.status == 200) {
-        document.getElementById("header").innerHTML = headerVar.responseText;
+          document.getElementById("header").innerHTML = headerVar.responseText;
+          
+          // Aquí llamas a las funciones que necesitas inicializar en el header
+          initHeaderFunctions();
       }
-    };
-    var footerVar = new XMLHttpRequest();
-    footerVar.open("GET", "footer.html", true);
-    footerVar.onreadystatechange = function () {
+  };
+
+  var footerVar = new XMLHttpRequest();
+  footerVar.open("GET", "footer.html", true);
+  footerVar.onreadystatechange = function () {
       if (footerVar.readyState == 4 && footerVar.status == 200) {
-        document.getElementById("footer").innerHTML = footerVar.responseText;
+          document.getElementById("footer").innerHTML = footerVar.responseText;
       }
-    };
-    headerVar.send();
-    footerVar.send();
+  };
+  
+  headerVar.send();
+  footerVar.send();
+}
+function initHeaderFunctions() {
+  const menuBtn = document.getElementById("menu-btn")
+  const navLinks = document.getElementById("nav-links")
+  const menuBtnIcon = menuBtn.querySelector("i")
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen =  navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen?"ri-close-line":"ri-menu-line "); 
+
+});
+
+navLinks.addEventListener("click", (e) =>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+  // Aquí puedes añadir más eventos o funcionalidades que dependan del header cargado
 }
